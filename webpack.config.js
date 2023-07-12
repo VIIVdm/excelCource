6d8fb41ed68bb23defd9f931218430aaf1901b1c
Viktoriya Baturina <vii.sb@mail.ru>
2023-07-12 15:10:49 +0300

    add clean plugin and source-map

--- webpack.config.js
+++ webpack.config.js
@@ -5,56 +5,69 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 
-module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        main: ['@babel/polyfill', './index.js']
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            // '@core': path.resolve(__dirname, 'src', 'core')
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new CopyPlugin({
-            patterns: [
-              { from: path.resolve(__dirname, 'src', 'favicon.ico'), 
-              to: path.resolve(__dirname, 'dist') },
-            ],
-          }),
-        new MiniCssExtractPlugin({
-            filename: '[name].bundle.css'
-        })
-    ],
-    module: {
-        rules: [
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                "css-loader",
-                "sass-loader",
+module.exports = (env, argv) => {
+    // проверить плагин на чистку dist файла
+    const isProd = argv.mode === 'production'
+    const isDev = !isProd
+
+    console.log('isProd', isProd)
+    console.log('isDev', isDev)
+
+    const filename = ext => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+
+    return {
+        context: path.resolve(__dirname, 'src'),
+        entry: {
+            main: ['@babel/polyfill', './index.js']
+        },
+        output: {
+            filename: filename('js'),
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+        },
+        resolve: {
+            extensions: ['.js'],
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                // '@core': path.resolve(__dirname, 'src', 'core')
+            }
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './index.html'
+            }),
+            new CopyPlugin({
+                patterns: [
+                  { from: path.resolve(__dirname, 'src', 'favicon.ico'), 
+                  to: path.resolve(__dirname, 'dist') },
+                ],
+              }),
+            new MiniCssExtractPlugin({
+                filename: filename('css')
+            })
+        ],
+        devtool: isDev ? 'source-map' : false,
+        module: {
+            rules: [
+              {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+              },
+              {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              }
             ],
           },
-          {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }
-        ],
-      },
+    }
 }
