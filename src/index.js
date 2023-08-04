81ea5c4a65a65180c5c4a919d7118991bb20b42a
Viktoriya Baturina <vii.sb@mail.ru>
2023-08-04 13:46:55 +0300

    add basic structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import {Excel} from '@/components/excel/Excel'
 import './scss/index.scss'
 
+const excel = new Excel('#app', {
+    components: []
+})
 
-console.log('Hello JS!')
+console.log('Excel', excel)
--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = (env, argv) => {
             extensions: ['.js'],
             alias: {
                 '@': path.resolve(__dirname, 'src'),
-                // '@core': path.resolve(__dirname, 'src', 'core')
+                '@core': path.resolve(__dirname, 'src', 'core')
             }
         },
         devServer: {
