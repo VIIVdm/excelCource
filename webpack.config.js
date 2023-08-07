42f240cd6b3ae89b7f31bed52a6e19ff1e099a8d
Viktoriya Baturina <vii.sb@mail.ru>
2023-08-07 18:09:16 +0300

    add html to framework

--- webpack.config.js
+++ webpack.config.js
@@ -79,7 +79,8 @@ module.exports = (env, argv) => {
                 use: {
                   loader: 'babel-loader',
                   options: {
-                    presets: ['@babel/preset-env']
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-class-properties']
                   }
                 }
               }
