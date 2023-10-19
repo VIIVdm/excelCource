49bc6297a7a6811186ea75901ce93aa5eab06c88
Viktoriya Baturina <vii.sb@mail.ru>
2023-10-19 15:46:19 +0300

    add a new logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,10 +3,21 @@ const CODES = {
     Z: 90
 }
 
-function createCell(_, col) {
-    return `
-    <div class="cell" contenteditable data-col="${col}"></div>
-    `
+// function createCell(_, col) {
+//     return `
+//     <div class="cell" contenteditable data-col="${col}"></div>
+//     `
+// }
+
+function toCell(row) {
+    return function(_, col) {
+        return `
+            <div class="cell" contenteditable 
+            data-col="${col}"
+            data-id="${row}:${col}">
+            </div>
+            `
+    }
 }
 
 function toColumn(col, index) {
@@ -47,13 +58,14 @@ export function createTable(rowsCount = 15) {
 
     rows.push(createRow(null, cols))
 
-    const cells = new Array(colsCount)
-    .fill('')
-    .map(createCell)
-    .join('')
 
-    for (let i = 0; i < rowsCount; i++) {
-        rows.push(createRow(i + 1, cells))
+    for (let row = 0; row < rowsCount; row++) {
+        const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell(row))
+        .join('')
+
+        rows.push(createRow(row + 1, cells))
     }
 
     return rows.join('')
